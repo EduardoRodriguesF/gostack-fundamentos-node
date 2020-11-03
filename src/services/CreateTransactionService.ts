c5bd1ff915659576fbc1631793b019f03da0c575
EduardoRodriguesF <eduaadorfernandes@gmail.com>
2020-11-03 09:52:51 -0300

    desafio concluido

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,15 +1,47 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
+interface Balance {
+  income: number;
+  outcome: number;
+  total: number;
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
+  private balance: Balance;
+
   constructor(transactionsRepository: TransactionsRepository) {
     this.transactionsRepository = transactionsRepository;
+    this.balance = transactionsRepository.getBalance();
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+    if (type === 'outcome') {
+      if (value > this.balance.total) {
+        throw Error('Insufficiente balance');
+      }
+      this.balance.outcome += value;
+    } else {
+      this.balance.income += value;
+    }
+
+    this.balance.total = this.balance.income - this.balance.outcome;
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
   }
 }
 
